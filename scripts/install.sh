adf6d0fbea794ef3e42a169a3620c7e26467e260
Leandro Gonaçalves <contato.leandrogoncalves@gmail.com>
2020-06-03 13:42:15 -0300

    Configuring the dev envirounment

---
+++ scripts/install.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+yarn create react-app modulo7
+yarn add eslint -D
+yarn eslint --init
+yarn
+
+yarn add prettier eslint-config-prettier eslint-plugin-prettier babel-eslint -D
+
+yarn add react-router-dom
+
+yarn add styled-components
+yarn add react-icons
+yarn add polished
+
+yarn add json-server -D
+yarn add axios
+yarn json-server server.json -p 3333 -w
+
+yarn add redux react-redux
+
+yarn add reactotron-react-js reactotron-redux
+
+yarn add immer
+
+yarn add redux-saga
+yarn add reactotron-redux-saga
+
+yarn add react-toastify
+yarn add history
+
+yarn add eslint-plugin-react-hooks -D
